ab81bfcb2399dc0d0f3e98d62e4e0f0aa85a99c3
cristian25cd <cd.copete461@uniandes.edu.co>
2014-10-11 09:50:55 -0500

    Clasen cliente

---
+++ src/cliente/Cliente.java
@@ -0,0 +1,79 @@
+package cliente;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/**
+ *  
+ */
+public class Cliente
+{
+    //-----------------------------------------------------------------
+    // Constantes
+    //-----------------------------------------------------------------
+
+
+
+    //-----------------------------------------------------------------
+    // Atributos
+    //-----------------------------------------------------------------
+	private Socket socket;
+	private BufferedReader lector;
+	private PrintWriter escritor;
+
+    //-----------------------------------------------------------------
+    // Constructores
+    //-----------------------------------------------------------------
+
+    /**
+     *  
+     */
+    public Cliente( )
+    {
+    	try {
+			socket = new Socket("157.253.199.185",9999);
+			lector = new BufferedReader(new InputStreamReader(socket.getInputStream())); 
+			escritor = new PrintWriter(socket.getOutputStream(), true);
+		} 
+    	catch (UnknownHostException e) {
+			e.printStackTrace();
+		}
+    	catch (IOException e) {
+			e.printStackTrace();
+		}
+    }
+
+    //-----------------------------------------------------------------
+    // Métodos
+    //-----------------------------------------------------------------
+    
+    
+    //-----------------------------------------------------------------
+    // Puntos de Extensión
+    //-----------------------------------------------------------------
+
+    /**
+     * Método para la extensión 1
+     * @return respuesta1
+     */
+    public String metodo1(String mensaje)
+    {
+    	escritor.println(mensaje);
+        return "Se envió el mensaje";
+    }
+
+    /**
+     * Método para la extensión2
+     * @return respuesta2
+     */
+    public String metodo2( )
+    {
+        return "Respuesta 2";
+    }
+
+
+}
--- src/cliente/Principal.java
+++
@@ -1,9 +0,0 @@
-package cliente;
-
-public class Principal {
-
-	public Principal() 
-	{
-		
-	}
-}
